e2993eaa455729991e49ddbcf642213ff9500de5
HSeung03 <lsh232986@naver.com>
2026-06-13 14:19:54 +0900

    fix: API URL 배포 서버로 변경

--- mobile/src/api/axios.js
+++ mobile/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const API_URL = 'http://localhost/api'
+const API_URL = 'http://98.81.210.162/api'
 
 const api = axios.create({
   baseURL: API_URL,
